92f8fa741a25bed22be99b2d378fd49b2d65abcb
Alexandra Pestruyeva <apestruy@gmail.com>
2020-01-06 04:39:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -22,6 +22,12 @@ class Application
           resp.write "#{item}\n"
         end
       end
+    elsif req.path.match(/add/)
+      next_item = req.params["item"]
+      
+      
+      
+      
     else
       resp.write "Path Not Found"
     end
